cd0820182ba01fb3777d3587ceb61346db59f0d0
ExtremeAntonis <68855179+ExtremeAntonis@users.noreply.github.com>
2022-01-25 21:40:30 +0200

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,17 +1,1 @@
-	local key = script.Parent.Parent.key
-	
-	script.Parent.MouseButton1Click:Connect(function()
-		if key.Text == QWDJHWQDWRTQEWTQU then
-			key.Text = "Correct Key"
-			wait(2)
-			script.Parent.Parent:Destroy()
-			print("Destroyed!")
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/ExtremeAntonis/KeySystemUI/main/All-Scripts-Obfuscated.lua"))()
-		elseif
-			key.Text ~= QWDJHWQDWRTQEWTQU then
-			key.Text = "Incorrect Key"
-			wait(1)
-			key.Text = ""
-		end
-	end)
-end
+QWDJHWQDWRTQEWTQU
